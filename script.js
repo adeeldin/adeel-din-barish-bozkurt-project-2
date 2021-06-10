232b6d033688c4773ce4aa0f4f7f3fc7ab1623c8
Adeel Din <adeeldin72@gmail.com>
2021-06-10 16:38:24 -0400

    added styling, fixed responsiveness, and cleaned up the javascript

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-
-
-//all books api
-const generateListURL = 'https://api.nytimes.com/svc/books/v3/lists/names'; //get book genres
-const specificBookISBN = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json' //get book information using isbn number
-const loadBooks = 'https://api.nytimes.com/svc/books/v3/lists.json'; //load books using genre
-
-//cover picture needs key value and size
-const picture = 'http://covers.openlibrary.org/b/$key/$value-$size.jpg'
-
 //our namespace
 const app = {};
 
+//all books api
+app.generateListURL = 'https://api.nytimes.com/svc/books/v3/lists/names'; //get book genres
+app.specificBookISBN = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json' //get book information using isbn number
+app.loadBooks = 'https://api.nytimes.com/svc/books/v3/lists.json'; //load books using genre
+app.apiKey = 'VGnDGaD8A3qh1qSrqo0ppSGLHoMcom3T'; //our api key
 
+
+//loading animation selector
 app.loadingAnimation = document.querySelector('.loadingAnimation');
 
 //array to hold specific amazon urls for the books that are displayed
 app.amazonLinksArray = [];
 
+//function used to populate the select option 
 app.populateGenreOptions = function () {
     const populateUrl = new URL('https://proxy.hackeryou.com'); //proxy url for cors is necessary 
     populateUrl.search = new URLSearchParams({
-        reqUrl: generateListURL,
-        // 'params[list]': 'hardcover-fiction',
-        'params[api-key]': 'VGnDGaD8A3qh1qSrqo0ppSGLHoMcom3T' //api key
-        // 'proxyHeaders[Some-Header]': 'goes here',
+        reqUrl: app.generateListURL,
+        'params[api-key]': app.apiKey //api key
+
     });
 
     //api call to populate genre section
@@ -39,23 +36,19 @@ app.populateGenreOptions = function () {
 
         })
         .then(data => {
-            const sortedArray = [];
+            const sortedArray = []; //array that will be used to sort our returns
 
             data['results'].forEach((value) => {
-                sortedArray.push(value.list_name);
+                sortedArray.push(value.list_name); //push fetched names to the array
             });
-            sortedArray.sort();
-            // console.log(sortedArray);
+            sortedArray.sort(); //sort the array
 
-            sortedArray.forEach((value) => {
-                const option = document.createElement('option');
-                option.value = value;
+            sortedArray.forEach((value) => { //loop through array and push the values into select 
+                const option = document.createElement('option'); //create option
+                option.value = value; //change value and text with content inside the array
                 option.textContent = value;
-                document.querySelector('#genre').append(option);
+                document.querySelector('#genre').append(option); //append the option
             });
-
-
-
 
         });
 }
@@ -63,24 +56,35 @@ app.populateGenreOptions = function () {
 app.searchByISBN = function (isbn, id) {
     const isbnURL = new URL('https://proxy.hackeryou.com'); //proxy url for cors is necessary 
     isbnURL.search = new URLSearchParams({
-        reqUrl: specificBookISBN,
+        reqUrl: app.specificBookISBN,
         'params[isbn]': isbn,
-        'params[api-key]': 'VGnDGaD8A3qh1qSrqo0ppSGLHoMcom3T' //api key
-        // 'proxyHeaders[Some-Header]': 'goes here',
+        'params[api-key]': app.apiKey //api key
+
     });
+
     //api call to populate the modal
-    fetch(isbnURL)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            // console.log(data);
-            const div = document.createElement('div');
-            div.classList.add('modal');
+    const search = function () { //function used to run the fetch inside the main function call so that if the fetch fails we can recall this function so far we are only failing when the proxy server fails, possible endless loop situation if fetch starts calling for something that will never return data
+
+        app.loadingAnimation.style.display = 'flex'; //display loading animation by returning to flex
+        fetch(isbnURL)
+            .then(response => {
+                if (response.ok) { //if data is returned continue
+                    return response.json()
+                } else { //recall the search function doing everything over until we get data
+                    setTimeout(() => {
+                        search();
+                    }, 2000); //wait 2 seconds
+
+                }
+            })
+            .then(data => {
+
+                const div = document.createElement('div'); //create div 
+                div.classList.add('modal'); //add class of modal
 
 
-
-            div.innerHTML = `<div class="modalScroll"><div class='innerContent'>
+                //div inner content is replaced with the necessary information from the api return to create the modal
+                div.innerHTML = `<div class="modalScroll"><div class='innerContent'>
             <button class="closeModal" onclick="app.closeModal(this)">&#10006</button>
             <p class='title'>${data['results'][0].title}</p>
             <img src="https://covers.openlibrary.org/b/ISBN/${data['results'][0]['isbns'][0].isbn10}-L.jpg" alt="The book cover for ${data['results'][0].title} by ${data['results'][0].author}">
@@ -102,21 +106,23 @@ app.searchByISBN = function (isbn, id) {
 
         </div>
         </div>`;
+                //append the div
+                document.querySelector('body').append(div);
+                app.loadingAnimation.style.display = 'none';  //remove loading animation
+            });
 
-            document.querySelector('body').append(div);
-
-        });
-
+    }
+    search();
 }
 
 app.searchBooks = function (genre) {
     app.loadingAnimation.style.display = 'flex';
     const populateUrl = new URL('https://proxy.hackeryou.com'); //proxy url for cors is necessary 
     populateUrl.search = new URLSearchParams({
-        reqUrl: loadBooks,
+        reqUrl: app.loadBooks,
         'params[list]': genre,
-        'params[api-key]': 'VGnDGaD8A3qh1qSrqo0ppSGLHoMcom3T' //api key
-        // 'proxyHeaders[Some-Header]': 'goes here',
+        'params[api-key]': app.apiKey //api key
+
     });
     //api call to populate the main page
     fetch(populateUrl)
@@ -126,7 +132,7 @@ app.searchBooks = function (genre) {
             }
         })
         .then(data => {
-            // console.log(data);
+
             data['results'].forEach((value, index) => {
                 const imgEl = document.createElement('li');
                 const image = document.createElement('img'); //used to check if image has successfully loaded
@@ -145,16 +151,6 @@ app.searchBooks = function (genre) {
                     image.src = `https://covers.openlibrary.org/b/ISBN/${value['isbns'][0].isbn10}-L.jpg?default=false`
                     app.amazonLinksArray.push(value.amazon_product_url);
 
-
-
-                    // image.addEventListener('error', function () {
-                    //     imgEl.innerHTML = `<p>${value['book_details'][0].title}</p>
-                    // <button class="buttonStyle" onclick="app.displayModal(this)" id=${index}><img id = "${value['isbns'][0].isbn10}" src="https://islandpress.org/sites/default/files/default_book_cover_2015.jpg" alt=""></button>`
-                    //     document.querySelector('.bookDisplay').append(imgEl);
-                    // }, true); //no idea what this true does
-
-
-                    // console.log(value['book_details'][0].author)
                     imgEl.innerHTML = `<p>${value['book_details'][0].title}</p>
                     <button class="buttonStyle" onclick="app.displayModal(this)" id=${index}><img id = "${value['isbns'][0].isbn10}" src="https://covers.openlibrary.org/b/ISBN/${value['isbns'][0].isbn10}-L.jpg" alt="The book cover for ${value['book_details'][0].title} ${value['book_details'][0].author !== '' ? `by ${value['book_details'][0].author}.` : ""}"></button>`
                     document.querySelector('.bookDisplay').append(imgEl);
@@ -164,7 +160,7 @@ app.searchBooks = function (genre) {
 
             })
 
-            app.loadingAnimation.style.display = 'none';
+            app.loadingAnimation.style.display = 'none'; //remove loading animation
         });
 
 }
@@ -190,16 +186,40 @@ app.addEventListeners = function () {
 
 
 }
+//function used to close the modal by changing its style
 app.closeModal = function (event) {
-    // console.log(`works`)
-    // console.log(event.parentNode.parentNode)
     event.parentNode.parentNode.parentNode.style.display = `none`;
 }
-//display model pop-up on tab & click
+
+//function used too call the search byISBN function which will eventually lead to displaying modal pop-up
 app.displayModal = function (event) {
     app.searchByISBN(event.childNodes[0].id, event.id); //call the search specific book api and pass the image id and the button id which will be used in the api call
 
 }
+
+
+// Back to top based on this https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+app.mybutton = document.getElementById("myBtn"); //button that is used for going back to top of page
+
+
+window.onscroll = function () { app.scrollFunction() }; //run this function on window scroll so when the user scrolls
+
+app.scrollFunction = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) { // When the user scrolls down 20px from the top of the document display the button
+        app.mybutton.style.display = "block";
+    } else { //or hide the button
+        app.mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+app.topFunction = function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
 //init function
 app.init = function () {
     app.populateGenreOptions();
@@ -212,6 +232,7 @@ app.init = function () {
     }, 1000);
 
 }
+
 
 //call init function
 app.init();
